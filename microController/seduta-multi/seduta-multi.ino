bf09783bf11e535eedd30276bd1561d5c7a7e1fc
massigualt <massimilia.gualtier3@studio.unibo.it>
2019-05-14 17:50:24 +0200

    Refactor seduta-multi

--- microController/seduta-multi/seduta-multi.ino
+++ microController/seduta-multi/seduta-multi.ino
@@ -1,30 +1,22 @@
 #include <HX711-multi.h>
 
 // Pins to the load cell amp
+#define DOUT1 1    // data pin to the first sensor
+#define DOUT2 2    // data pin to the second sensor
+#define DOUT3 3    // data pin to the third sensor
+#define DOUT4 4     // data pin to the fourth sensor
 #define CLK 5      // clock pin to the load cell amp
-#define DOUT1 1    // data pin to the first lca
-#define DOUT2 2    // data pin to the second lca
-#define DOUT3 3    // data pin to the third lca
-#define DOUT4 4     // data pin to the fourth lca
-
-#define BOOT_MESSAGE "MIT_ML_SCALE V0.8"
-
 #define TARE_TIMEOUT_SECONDS 4
 
 byte DOUTS[4] = {DOUT1, DOUT2, DOUT3, DOUT4};
-
 #define CHANNEL_COUNT sizeof(DOUTS)/sizeof(byte)
-
 long int results[CHANNEL_COUNT];
 
 HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(BOOT_MESSAGE);
   Serial.flush();
-  pinMode(11,OUTPUT);
-  
   tare();
 }
 
@@ -36,18 +28,18 @@ void tare() {
   }
 }
 
+//this is for sending raw data, for where everything else is done in processing
 void sendRawData() {
   scales.read(results);
   for (int i=0; i<scales.get_count(); ++i) {;
     Serial.print( -results[i]);
     Serial.print( (i!=scales.get_count()-1)?",":"\n");
-  }  
+  }
   delay(10);
 }
 
 void loop() {
-  sendRawData(); //this is for sending raw data, for where everything else is done in processing
-
+  sendRawData();
   //on serial data (any data) re-tare
   if (Serial.available()>0) {
     while (Serial.available()) {
