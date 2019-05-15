c5a4d53d3a063e0d17b9bc06338153d915e8582b
massigualt <massimilia.gualtier3@studio.unibo.it>
2019-05-15 09:52:24 +0200

    Updated data_collection script for read value by serial

--- raspberry/data_collection.py
+++ raspberry/data_collection.py
@@ -15,21 +15,20 @@ postura = int(input())
 
 print("Peso: " + str(peso) + " Postura: " + str(postura))
 
-
-# read from Arduino
-
-#inputSeduta = serDevSeduta.read()
-#inputSchienale = serDevSchienale.read()
-
-#receivedData = inputSeduta + "#" + inputSchienale
-
-
 unique_filename = str(postura)+"/"+str(uuid.uuid4())+".csv"
 
 with open("dataset/{}".format(unique_filename), 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     filewriter.writerow(['seduta1', 'seduta2', 'seduta3', 'seduta4', 'schienale1', 'schienale2', 'schienale3', 'postura'])
     for i in range(0,5):
+        # read five relevations from Arduino
+        #serDevSeduta = serial.Serial('/dev/ttyACM0', 115200)
+        #serDevSchienale = serial.Serial('/dev/ttyACM1', 115200)
+
+        #inputSeduta = serDevSeduta.readline()
+        #inputSchienale = serDevSchienale.readline()
+
+        #receivedData = inputSeduta + "#" + inputSchienale
         receivedData = "1#2#3#4#5#6#7"
         data = receivedData.split("#")
 
@@ -39,26 +38,21 @@ with open("dataset/{}".format(unique_filename), 'w', newline='') as csvfile:
             value = float(temp)
             equalizedData.append(value/peso)
 
-        equalizedData.append(postura)   
+        equalizedData.append(postura)
         filewriter.writerow(equalizedData)
-        print("take data")
+        print("Write row")
         time.sleep(5)
 
+'''
+print ("Read input " + input.decode("utf-8") + " from Arduino")
+write something back
+ser.write(b'A')
 
-#print ("Read input " + input.decode("utf-8") + " from Arduino")
+read response back from Arduino
+for i in range (0,3):
+        input = ser.read()
 
+        input_number = ord(input)
 
-# write something back
-#ser.write(b'A')
-
-
-
-# read response back from Arduino
-
-#for i in range (0,3):
-
- #       input = ser.read()
-
-  #      input_number = ord(input)
-
-   #     print ("Read input back: " + str(input_number))
+        print ("Read input back: " + str(input_number))
+'''
